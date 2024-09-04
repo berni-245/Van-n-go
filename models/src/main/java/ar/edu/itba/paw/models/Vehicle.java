08922f4121aa815acbc072444b82ecf29ac2b0db
Franco Morroni <fmorroni99@gmail.com>
2024-09-04 06:18:07 -0300

    fix: record class doesn't work due to non conventional getters

--- models/src/main/java/ar/edu/itba/paw/models/Vehicle.java
+++ models/src/main/java/ar/edu/itba/paw/models/Vehicle.java
@@ -1,4 +1,37 @@
 package ar.edu.itba.paw.models;
 
-public record Vehicle(long id, long driverId, String plateNumber, double volume, String description) {
+public class Vehicle {
+    private final long id;
+    private final long driverId;
+    private final String plateNumber;
+    private final double volume;
+    private final String description;
+
+    public Vehicle(long id, long driverId, String plateNumber, double volume, String description) {
+        this.id = id;
+        this.driverId = driverId;
+        this.plateNumber = plateNumber;
+        this.volume = volume;
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public long getDriverId() {
+        return driverId;
+    }
+
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
