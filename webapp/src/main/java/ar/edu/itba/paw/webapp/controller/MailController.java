660333f80e21e4bb26da59def3ccdd036f3b6f00
Tobias Pugliano <tpugliano@itba.edu.ar>
2024-09-10 13:23:37 -0300

    Feat: add requested service mails.

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/MailController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/MailController.java
@@ -2,10 +2,15 @@ package ar.edu.itba.paw.webapp.controller;
 
 
 import ar.edu.itba.paw.services.MailService;
+import ar.edu.itba.paw.webapp.form.DriverForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Date;
 
 @RequestMapping(method = RequestMethod.POST)
 @Controller
