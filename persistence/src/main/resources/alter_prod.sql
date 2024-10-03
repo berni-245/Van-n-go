cb7d3aeaa096bb675a52b03224b8f17a31f9f4cd
berni <bernizapico@gmail.com>
2024-10-03 18:36:08 -0300

    feat: added migration code to alter_prod.sql

--- persistence/src/main/resources/alter_prod.sql
+++ persistence/src/main/resources/alter_prod.sql
@@ -4,13 +4,9 @@ alter table app_user
     add column pfp int references image (id) on delete set null;
 alter table vehicle
     add column img_id int references image (id) on delete set null;
-alter table reservation
-    add column proof_of_payment int references image (id) on delete set null;
 
 alter table driver
     add column rating double precision;
-alter table booking
-    alter column rating type int;
 
 alter table weekly_availability
     rename to weekly_availability_old;
@@ -39,3 +35,41 @@ from vehicle_weekly_zone vwz
                  hb.t_end != '00:00'
 order by vehicle_id, zone_id, week_day, hb.id
 on conflict do nothing;
+
+alter table booking
+    rename to booking_old;
+
+alter table reservation
+    rename to reservation_old;
+
+create table if not exists booking
+(
+    id               serial primary key,
+    date             date,
+    hour_start_id    int     not null references hour_block (id) on delete cascade,
+    hour_end_id      int     not null references hour_block (id) on delete cascade,
+    client_id        int     not null references client (user_id) on delete cascade,
+    vehicle_id       int     not null references vehicle (id) on delete cascade,
+    is_confirmed     boolean not null,
+    proof_of_payment int     references image (id) on delete set null,
+    rating           int,
+    review           text
+);
+
+insert into booking (date, hour_start_id, hour_end_id, client_id, vehicle_id, is_confirmed)
+select bo.date,
+       min(wa.hour_block_id) as hour_start_id,
+       min(wa.hour_block_id) as hour_end_id,
+       re.client_id,
+       min(ve.id)            as vehicle_id,
+       re.is_confirmed
+from booking_old bo
+         join reservation_old re on bo.id = re.booking_id
+         join vehicle ve on re.driver_id = ve.driver_id
+         join weekly_availability wa on ve.id = wa.vehicle_id
+where wa.week_day = (select case
+                                when extract(dow from bo.date) = 0 then 7
+                                else extract(dow from bo.date) end)
+group by bo.id, bo.date, client_id, re.is_confirmed
+order by date
+;
