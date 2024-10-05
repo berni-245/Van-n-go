83829c4102b73063d49190e7d462750e46bf7bc0
berni <bernizapico@gmail.com>
2024-10-05 20:24:57 -0300

    feat: added HourInterval model

---
+++ models/src/main/java/ar/edu/itba/paw/models/HourInterval.java
@@ -0,0 +1,47 @@
+package ar.edu.itba.paw.models;
+
+import java.time.LocalTime;
+
+public class HourInterval {
+    private final int startHour;
+    private final int endHour;
+    private final int hourCount;
+
+    public HourInterval(int startHour, int endHour) {
+        if(! ((0 <= startHour) && (startHour < endHour) && (endHour <= 24)) )
+            throw new IllegalArgumentException("Start time must be before end time");
+        this.startHour = startHour;
+        this.endHour = endHour;
+        this.hourCount = endHour - startHour;
+    }
+
+    // Only uses the hours for interval
+    public HourInterval(LocalTime startTime, LocalTime endTime) {
+        this(startTime.getHour(), endTime.getHour() == 0 ? 24 : endTime.getHour());
+    }
+
+    // Only uses the hours for interval
+    public HourInterval(String startTime, String endTime) {
+        this(LocalTime.parse(startTime), LocalTime.parse(endTime));
+    }
+
+    public int getStartHour() {
+        return startHour;
+    }
+
+    public int getEndHour() {
+        return endHour;
+    }
+
+    public int getHourCount() {
+        return hourCount;
+    }
+
+    public int getStartHourBlockId() {
+        return startHour + 1;
+    }
+
+    public int getEndHourBlockId() {
+        return endHour;
+    }
+}
