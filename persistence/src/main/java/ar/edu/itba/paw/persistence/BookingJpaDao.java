a2bf1c1600763ead7c1ffc4ecf4f932a2019f9e9
tobias <tpugliano@itba.edu.ar>
2024-10-29 17:05:50 -0300

    chore: add implementation on BookingJpaDao

--- persistence/src/main/java/ar/edu/itba/paw/persistence/BookingJpaDao.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/BookingJpaDao.java
@@ -58,67 +58,109 @@ public class BookingJpaDao implements BookingDao {
 
     @Override
     public void rejectBooking(long bookingId) {
-
+        Optional<Booking> booking = getBookingById(bookingId);
+        if(booking.isEmpty()) {
+            throw new IllegalArgumentException("Booking with id " + bookingId + " not found"); //TODO: crear exceptoin propia
+        }
+        booking.get().setState(BookingState.REJECTED);
     }
 
     @Override
     public Optional<Booking> getBookingById(long bookingId) {
-        return Optional.empty();
+        return Optional.ofNullable(em.find(Booking.class, bookingId));
     }
 
-    @Override
+    @Override //TODO: revisar al migrar Driver a JPA
+    //TODO: Agregar paginacion!
     public List<Booking> getDriverBookings(long driverId, int offset) {
-        return List.of();
+        Driver driver = em.find(Driver.class, driverId);
+        if(driver == null) {
+            //throws...
+        }
+        TypedQuery<Booking> query = em.createQuery("From Booking as b where b.driver = :driver", Booking.class); //TODO: revisar si hace el equals de java
+        query.setParameter("driver", driver);
+        return query.getResultList();
     }
 
     @Override
     public List<Booking> getDriverHistory(long driverId, int offset) {
-        return List.of();
+        Driver driver = em.find(Driver.class, driverId);
+        if(driver == null) {
+            //throws...
+        }
+        TypedQuery<Booking> query = em.createQuery("From Booking as b where b.driver = :driver and b.date < CURRENT_DATE ", Booking.class);
+        query.setParameter("driver", driver);
+        return query.getResultList();
     }
 
     @Override
     public int getDriverBookingCount(long driverId) {
-        return 0;
+        return getDriverBookings(driverId, 0).size(); //TODO: paginacion!
     }
 
     @Override
     public int getDriverHistoryCount(long driverId) {
-        return 0;
+        return getDriverHistory(driverId, 0).size(); //TODO: paginacion!
     }
 
     @Override
     public List<Booking> getBookingsByVehicle(long vehicleId) {
-        return List.of();
+        Vehicle vehicle = em.find(Vehicle.class, vehicleId);
+        if(vehicle == null) {
+            //throws...
+        }
+        TypedQuery<Booking> query = em.createQuery("From Booking as b where b.vehicle = :vehicle", Booking.class);
+        query.setParameter("vehicle", vehicle); //TODO: hacer vehicle equals
+        return query.getResultList();
     }
 
     @Override
     public List<Booking> getBookingsByVehicleAndDate(long vehicleId, LocalDate date) {
-        return List.of();
+        Vehicle vehicle = em.find(Vehicle.class, vehicleId);
+        if(vehicle == null) {
+            //throws...
+        }
+        TypedQuery<Booking> query = em.createQuery("From Booking as b where b.vehicle = :vehicle and b.date = :date", Booking.class);
+        query.setParameter("vehicle", vehicle);
+        query.setParameter("date", date);
+        return query.getResultList();
     }
 
     @Override
     public List<Booking> getClientBookings(long clientId, int offset) {
-        return List.of();
+        Client client = em.find(Client.class, clientId);
+        if(client == null) {
+            //throws...
+        }
+        TypedQuery<Booking> query = em.createQuery("From Booking as b where b.client = :client", Booking.class);
+        query.setParameter("client", client); //TODO: hacer equals driver
+        return query.getResultList();
     }
 
     @Override
     public int getClientBookingCount(long clientId) {
-        return 0;
+        return getClientBookings(clientId, 0).size();
     }
 
     @Override
     public List<Booking> getClientHistory(long clientId, int offset) {
-        return List.of();
+        Client client = em.find(Client.class, clientId);
+        if(client == null) {
+            //throws...
+        }
+        TypedQuery<Booking> query = em.createQuery("From Booking as b where b.client = :client and b.date < CURRENT_DATE ", Booking.class);
+        query.setParameter("client", client);
+        return query.getResultList();
     }
 
     @Override
     public int getClientHistoryCount(long clientId) {
-        return 0;
+        return getClientHistory(clientId, 0).size();
     }
 
     @Override
     public void setRatingAndReview(long bookingId, int rating, String review) {
-
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
