71b48b2d3a9a642c4ec276437f0ce96b8696ea31
berni <bernizapico@gmail.com>
2024-11-02 20:51:06 -0300

    feat: added some custom exception to bookingJpaDao

---
+++ models/src/main/java/ar/edu/itba/paw/exceptions/ClientAlreadyAppointedException.java
@@ -0,0 +1,7 @@
+package ar.edu.itba.paw.exceptions;
+
+public class ClientAlreadyAppointedException extends RuntimeException{
+    public ClientAlreadyAppointedException() {
+        super("The client already appointed for that vehicle at that zone and time\n");
+    }
+}
