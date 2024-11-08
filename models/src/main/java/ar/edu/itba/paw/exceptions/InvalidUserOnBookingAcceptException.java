d860ae3e79b193db264b4e07ece2277019441876
berni <bernizapico@gmail.com>
2024-11-08 17:39:17 -0300

    fix: fixed typo in exception message

--- models/src/main/java/ar/edu/itba/paw/exceptions/InvalidUserOnBookingAcceptException.java
+++ models/src/main/java/ar/edu/itba/paw/exceptions/InvalidUserOnBookingAcceptException.java
@@ -2,6 +2,6 @@ package ar.edu.itba.paw.exceptions;
 
 public class InvalidUserOnBookingAcceptException extends RuntimeException {
     public InvalidUserOnBookingAcceptException() {
-        super("toast.invalid.user.booking.accept=The booking you are trying to accept is not yours\n");
+        super("The booking you are trying to accept is not yours\n");
     }
 }
