7b32744ad662e782bcd3c2e803a7b25cc888ba0c
berni <bernizapico@gmail.com>
2024-11-09 18:44:23 -0300

    chore: removed finished todo

--- persistence/src/main/resources/alter_prod.sql
+++ persistence/src/main/resources/alter_prod.sql
@@ -2,8 +2,6 @@ update app_user
 set language = 'SPANISH'
 where language is null;
 
--- TODO add constraints to vehicle to make not null plate_number, volume_m3, description
-
 ALTER TABLE vehicle
     ALTER COLUMN plate_number SET NOT NULL,
     ALTER COLUMN volume_m3 SET NOT NULL,
