94d026ec8629cbb69bc67aa2c0ce5136150dd440
berni <bernizapico@gmail.com>
2024-11-09 14:15:59 -0300

    chore: added function to find integer ids in alter prod

--- persistence/src/main/resources/alter_prod.sql
+++ persistence/src/main/resources/alter_prod.sql
@@ -5,3 +5,25 @@ set language = 'SPANISH'
 where language is null;
 
 -- TODO add constraints to vehicle to make not null plate_number, volume_m3, description
+
+/*
+
+ SELECT distinct
+    c.relname AS table_name,
+    a.attname AS column_name
+FROM
+    pg_class c
+JOIN
+    pg_attribute a ON c.oid = a.attrelid
+JOIN
+    pg_type t ON a.atttypid = t.oid
+JOIN
+    pg_constraint con ON con.conrelid = c.oid
+JOIN
+    unnest(con.conkey) AS conkey_pos ON conkey_pos = a.attnum
+WHERE
+    t.typname = 'int4'  -- serial es un alias de int4
+    AND c.relkind = 'r'  -- solo tablas regulares
+    AND c.relname NOT like 'pg_%' ;
+
+ */
