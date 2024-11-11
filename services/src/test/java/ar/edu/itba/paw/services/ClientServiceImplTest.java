5bc59598532d015121e28e5799679287b3ac76b8
berni <bernizapico@gmail.com>
2024-11-11 16:05:50 -0300

    feat: added driver service test

--- services/src/test/java/ar/edu/itba/paw/services/ClientServiceImplTest.java
+++ services/src/test/java/ar/edu/itba/paw/services/ClientServiceImplTest.java
@@ -15,21 +15,11 @@ import static org.junit.Assert.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ClientServiceImplTest {
-
-    @Mock
-    private UserDao<Client> userDao;
-    @Mock
-    private ClientDao clientDao;
-    @Mock
-    private PasswordEncoder passwordEncoder;
+    //This unused Service needs to exist for the test
     @Mock
     private MailService mailService;
     @Mock
     private BookingDao bookingDao;
-    @Mock
-    private VehicleDao vehicleDao;
-    @Mock
-    private ZoneDao zoneDao;
 
     @InjectMocks
     private ClientServiceImpl clientService;
@@ -44,7 +34,8 @@ public class ClientServiceImplTest {
         final Driver existingDriver = spy(Driver.class);
         existingBooking.setState(BookingState.ACCEPTED);
         existingDriver.setLanguage(Language.ENGLISH);
-        existingVehicle.setDriver(existingDriver);
+
+        when(existingVehicle.getDriver()).thenReturn(existingDriver);
         existingBooking.setVehicle(existingVehicle);
 
         when(bookingDao.getClientBookingById(existingClient, BOOKING_ID)).thenReturn(existingBooking);
@@ -64,9 +55,7 @@ public class ClientServiceImplTest {
     public void testCancelForbiddenBooking() {
         final Booking existingBooking = spy(Booking.class);
         final Client existingClient = spy(Client.class);
-        final Vehicle existingVehicle = spy(Vehicle.class);
         existingBooking.setState(BookingState.FINISHED); // Since not ACCEPTED, it will throw an exception
-        existingBooking.setVehicle(existingVehicle);
 
         when(bookingDao.getClientBookingById(existingClient, BOOKING_ID)).thenReturn(existingBooking);
 
