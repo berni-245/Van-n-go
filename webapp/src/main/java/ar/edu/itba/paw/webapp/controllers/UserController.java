6cbac86ec427ac29996a530935e304ad2d00dbe4
berni <bernizapico@gmail.com>
2025-07-28 19:02:48 -0300

    feat: added configuration and example for exception mappers to response status

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controllers/UserController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controllers/UserController.java
@@ -1,7 +1,6 @@
 package ar.edu.itba.paw.webapp.controllers;
 
-import ar.edu.itba.paw.models.User;
-import ar.edu.itba.paw.services.UserService;
+import ar.edu.itba.paw.exceptions.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -9,7 +8,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.*;
-import java.util.ArrayList;
 import java.util.List;
 
 // curl -H'Content-type:application/json' http://localhost:8080/api/users -v
@@ -31,6 +29,13 @@ public class UserController {
     public Response getMethod() {
         List<String> list = List.of("Hola", "jaja!", "prueba");
 
-        return Response.ok(new GenericEntity<List<String>>(list) {}).build();
+        return Response.ok(new GenericEntity<>(list) {}).build();
+    }
+
+    @GET
+    @Path("/test")
+    @Produces(value = {MediaType.APPLICATION_JSON})
+    public Response testMapper() { // to test the Exception to Response mapper
+        throw new UserNotFoundException();
     }
 }
