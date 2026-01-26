0c4c987773cd742249b9275c342159a7048a8d38
Franco Morroni <fmorroni99@gmail.com>
2026-01-26 19:05:46 -0300

    fix: changed the name of a variable

--- webapp/src/main/java/ar/edu/itba/paw/webapp/dto/ErrorDTO.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/dto/ErrorDTO.java
@@ -3,15 +3,15 @@ package ar.edu.itba.paw.webapp.dto;
 public class ErrorDTO {
 
     private String type;
-    private String msg;
+    private String description;
 
     public ErrorDTO() {
 
     }
 
-    public ErrorDTO(ErrorType type, String msg) {
+    public ErrorDTO(ErrorType type, String description) {
         this.type = type.name();
-        this.msg = msg;
+        this.description = description;
     }
 
     public String getType() {
@@ -22,11 +22,11 @@ public class ErrorDTO {
         this.type = type;
     }
 
-    public String getMsg() {
-        return msg;
+    public String getDescription() {
+        return description;
     }
 
-    public void setMsg(String msg) {
-        this.msg = msg;
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
